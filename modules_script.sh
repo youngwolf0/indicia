75f5e7178796d69bc2536a9e6fb73e5c8f432f57
Colin McTaggart <shockwaves6@aol.com>
2015-06-12 04:00:33 +0100

    Moved contrib folders to the appropriate directory, it was annoying me that I'd forgotten earlier.

--- modules_script.sh
+++
@@ -1,9 +0,0 @@
-drush dis overlay
-drush dis toolbar
-drush en module_filter -y
-drush en admin_menu -y
-drush en context_ui -y
-drush en devel -y
-drush en indicia_logger -y
-drush en features -y
-drush en homepage_context -y
